0fec5403049dda6d707fbfe19bbf91953a393eb8
eunji <eunji@coreintec.com>
2022-01-24 13:27:46 +0900

    관리자 mapping url 변경

--- src/main/java/com/debrains/debrainsApi/controller/admin/AdminController.java
+++ src/main/java/com/debrains/debrainsApi/controller/admin/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/auth")
+@RequestMapping("/root")
 @RequiredArgsConstructor
 public class AdminController {
 
