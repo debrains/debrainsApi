c42965de7c72ac2b46f25c03b8d2537ac5e84743
eunji <eunji@coreintec.com>
2022-03-22 17:53:13 +0900

    메일 발송 기능

---
+++ src/main/java/com/debrains/debrainsApi/repository/MailRepository.java
@@ -0,0 +1,8 @@
+package com.debrains.debrainsApi.repository;
+
+import com.debrains.debrainsApi.entity.Mail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface MailRepository  extends JpaRepository<Mail, Long> {
+}
