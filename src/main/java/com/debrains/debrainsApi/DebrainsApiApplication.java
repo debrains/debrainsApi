473b80125ef04c63b2c4ab04c5b86911c7ae778d
SANGIL PARK <europani1212@gmail.com>
2022-04-02 08:41:11 +0900

    DEB-102 appspec.yml, deploy.sh typo / 외부 application yml 선택 (#50)

--- src/main/java/com/debrains/debrainsApi/DebrainsApiApplication.java
+++ src/main/java/com/debrains/debrainsApi/DebrainsApiApplication.java
@@ -2,13 +2,19 @@ package com.debrains.debrainsApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DebrainsApiApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "optional:classpath:application.yml,"
+			+ "optional:/usr/local/debrainsApi/application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(DebrainsApiApplication.class, args);
+		new SpringApplicationBuilder(DebrainsApiApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
